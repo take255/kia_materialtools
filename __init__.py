30c94d71123d21367cee96d85dd75f8c88d73ec7
takeoh <take255@gmail.com>
2020-03-18 19:32:59 +0900

    commit

--- __init__.py
+++ __init__.py
@@ -32,47 +32,66 @@ from . import cmd
 imp.reload(utils)
 imp.reload(cmd)
 
+bl_info = {
+"name": "kiamaterialtools",
+"author": "kisekiakeshi",
+"version": (0, 1),
+"blender": (2, 80, 0),
+"description": "kiamaterialtools",
+"category": "Object"}
 
-class KIAMATERIAL_Props_OA(PropertyGroup):
-    material_color : FloatVectorProperty( name = "mycolor",subtype = "COLOR",size = 4, min=0.0, max=1.0, default=(0.75,0.0,0.8,1.0) )
+#MATERIAL_TYPE = ( ('METAL','METAL','') , ('LEATHER','LEATHER','') , ('CLOTH','CLOTH','') , ('OTHERS','OTHERS','') , ('BUFFER','BUFFER','') )
+
+MATERIAL_TYPE = []
+for vc in cmd.VERTEX_COLOR:
+    MATERIAL_TYPE.append( (vc,vc,'') )
+
+class KIAMATERIALTOOLS_Props_OA(PropertyGroup):
+    material_color : FloatVectorProperty( name = "Select Face",subtype = "COLOR",size = 4, min=0.0, max=1.0, default=(0.75,0.0,0.8,1.0) )
+    material_type : EnumProperty(items = MATERIAL_TYPE , name = 'type' ,update = cmd.change_mattype)
+    material_index : IntProperty( name = "number", min=0, max=10, default=1 )
+
 
 
 #---------------------------------------------------------------------------------------
 #Vertex Color List
 #---------------------------------------------------------------------------------------
 
-class VertexColorItem(bpy.types.PropertyGroup):
-    color_value = bpy.props.FloatVectorProperty(
-        name="Color", 
-        subtype="COLOR", 
-        min=0.0, 
-        max=1.0,
-        # get=get_color_value,
-        # set=set_color_value
-    )
+class KIAMATERIALTOOLS_UL_uilist(UIList):
+    
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        if self.layout_type in {'DEFAULT', 'COMPACT'}:
 
-class VertexColorlist(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
-        layout.label("", icon="COLOR")
-        layout.prop(item, "name",  "")
-        layout.prop(item, "color_enum", "")
-        layout.prop(item, "color_value", "")
+            #item.nameが表示される
+            #layout.prop(item, "bool_val", text = "")
+            #layout.prop(item, "name", text="", emboss=False, icon_value=icon)
+            layout.prop(item, "name", text="", emboss=False, icon_value=icon)
+            layout.prop(item, "color",text= "")
+
+        elif self.layout_type == 'GRID':
+            layout.alignment = 'CENTER'
+            layout.label(text="", icon_value=icon)
 
 #---------------------------------------------------------------------------------------
 #マテリアル関連ツール
 #---------------------------------------------------------------------------------------
-class KIATOOLS_MT_materialtools(Operator):
-    bl_idname = "kiatools.materialtools"
-    bl_label = "material"
+class KIAMATERIALTOOLS_PT_materialtools(utils.panel):
+    # bl_idname = "kiatools.materialtools"
+    bl_label = "material tools"
 
-    def execute(self, context):
-        return{'FINISHED'}
+    # def execute(self, context):
+    #     #cmd.add(cmd.VERTEX_COLOR[0])
+
+    #      return{'FINISHED'}
 
     def invoke(self, context, event):
+        cmd.add(cmd.VERTEX_COLOR[0])
         return context.window_manager.invoke_props_dialog(self)
 
     def draw(self, context):
-        props = bpy.context.scene.kiatools_oa
+        props = bpy.context.scene.kiamaterialtools_oa
+        ui_list = context.window_manager.kiamaterialtools_list
+
         layout=self.layout
 
         row = layout.row(align=False)
@@ -80,30 +99,124 @@ class KIATOOLS_MT_materialtools(Operator):
 
         box = col.box()
         box.label(text = 'vertex color')
+
         col = box.column()
         row = col.row()        
         row.prop(props, "material_type" )
-        row.prop(props, "material_index" )
+        #row.prop(props, "material_index" )
+
+        col.template_list("KIAMATERIALTOOLS_UL_uilist", "", ui_list, "itemlist", ui_list, "active_index", rows=8)
 
         row = col.row()
-        row.operator("kiatools.material_assign_vertex_color", text = 'assign').mode = 0
-        row.operator("kiatools.material_assign_vertex_color", text = 'assign(selected)').mode = 1
-        row.operator("kiatools.material_convert_vertex_color")
+        row.operator("kiamaterialtools.assign_vertex_color", text = 'assign').mode = 0
+        row.operator("kiamaterialtools.assign_vertex_color", text = 'assign(selected)').mode = 1
 
         row = col.row()
-        row.operator("kiatools.pick_vertex_color", text = 'pick').mode = True
-        row.operator("kiatools.pick_vertex_color", text = 'put').mode = False
+        row.operator("kiamaterialtools.convert_vertex_color")
+
+
 
         box = col.box()
-        box.label(text = 'vertex color')
+        #box.label(text = 'vertex color')
         box.prop(props, "material_color")
 
-        box.template_list(
-            "SCENE_UL_my_color_list", 
-            "my_color_list", 
-            settings,
-            "colors",
-            settings,
-            "active_color_index",
-            type='DEFAULT'
+        row = box.row()
+        row.operator("kiamaterialtools.pick_vertex_color", text = 'pick').mode = True
+        row.operator("kiamaterialtools.pick_vertex_color", text = 'assign').mode = False
+
+
+
+        # box.template_list(
+        #     "SCENE_UL_my_color_list", 
+        #     "my_color_list", 
+        #     settings,
+        #     "colors",
+        #     settings,
+        #     "active_color_index",
+        #     type='DEFAULT'
+        # )
+        
+
+
+class KIAMATERIALTOOLS_Props_item(PropertyGroup):
+    name : StringProperty(get=cmd.get_item, set=cmd.set_item)
+    color : FloatVectorProperty(
+        name = "color",subtype = "COLOR",size = 4, min=0.0, max=1.0,
+        get=cmd.get_color_value,
+        set=cmd.set_color_value
         )
+    #bool_val : BoolProperty( update = cmd.showhide )
+
+bpy.utils.register_class(KIAMATERIALTOOLS_Props_item)
+
+#---------------------------------------------------------------------------------------
+class KIAMATERIALTOOLS_Props_list(PropertyGroup):
+    active_index : IntProperty()
+    itemlist : CollectionProperty(type=KIAMATERIALTOOLS_Props_item)#アイテムプロパティの型を収めることができるリストを生成
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------
+#マテリアルツール
+#---------------------------------------------------------------------------------------
+class KIAMATERIALTOOLS_OT_assign_vertex_color(Operator):
+    bl_idname = "kiamaterialtools.assign_vertex_color"
+    bl_label = "assign vtxcolor"
+    mode : IntProperty()
+    def execute(self, context):
+        cmd.assign_vertex_color(self.mode)
+        return {'FINISHED'}
+
+class KIAMATERIALTOOLS_OT_convert_vertex_color(Operator):
+    """シェーダーはPrincipled BSDFにすること"""
+    bl_idname = "kiamaterialtools.convert_vertex_color"
+    bl_label = "convert vtxcolor"
+    def execute(self, context):
+        cmd.convert_vertex_color()
+        return {'FINISHED'}
+
+class KIAMATERIALTOOLS_OT_pick_vertex_color(Operator):
+    """ First, select polugon face (not vertex) and execute this command."""
+    bl_idname = "kiamaterialtools.pick_vertex_color"
+    bl_label = ""
+    mode : BoolProperty()
+    def execute(self, context):
+        cmd.pick_vertex_color(self.mode)
+        return {'FINISHED'}
+
+
+
+
+
+
+classes = (
+    KIAMATERIALTOOLS_Props_OA,
+    KIAMATERIALTOOLS_UL_uilist,
+    KIAMATERIALTOOLS_Props_list,
+
+    KIAMATERIALTOOLS_PT_materialtools,
+    KIAMATERIALTOOLS_OT_assign_vertex_color,
+    KIAMATERIALTOOLS_OT_convert_vertex_color,
+    KIAMATERIALTOOLS_OT_pick_vertex_color
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.kiamaterialtools_oa = PointerProperty(type=KIAMATERIALTOOLS_Props_OA)
+    bpy.types.WindowManager.kiamaterialtools_list = PointerProperty(type=KIAMATERIALTOOLS_Props_list)
+    #cmd.add(cmd.VERTEX_COLOR[0])
+
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.kiamaterialtools_oa
+    del bpy.types.WindowManager.kiamaterialtools_list
--- cmd.py
+++ cmd.py
@@ -7,12 +7,64 @@ import imp
 from . import utils
 imp.reload(utils)
 
+
 VERTEX_COLOR = {
-'METAL'  :("ff0000","00ffff","993333","339982","ff9999","99ffe9","ffdb99","e4e54c"),
+'' : '' ,
+'SET01'  :("ff0000","ffff00","00ff00","00FFFF","0000FF","FF00FF","000000","ffffff"),
+'SET02'  :("FEFEFE","9A9A9A","676767","414141","262626","0C0C0C","040404","000000"),
+'SET03'  :("220000","550000","7D0000","A40001","E60013","E60013","EC6841","FAD6AD"),
+'SET04'  :("322B00","625B00","8A8100","B7AB00","FFF100","FFF45C","FFF899","FFF899"),
+
+'METAL'  :("ff0000","00ffff","FFA500","333339","ff9999","99ffe9","ffdb99","e4e54c"),
 'LEATHER':("fff900","0000ff","999933","333399","ffff99","99bcff","062d0c","ffa500"),
 'CLOTH'  :("00ff00","ff00ff","339943","753399","ff6600","db99ff","0d0d59","ec008c"),
 'OTHER'  :("590d29","56493d","798ba8","b58f7b","32590d","3e0d59","41410d","af4ce5"),
+
 }
+
+#---------------------------------------------------------------------------------------
+#List Rerated Commands
+#---------------------------------------------------------------------------------------
+
+#リストからアイテムを取得
+def get_item(self):
+    return self["name"]
+
+#リストに選択を登録する
+def set_item(self, value):
+    self["name"] = value
+
+def change_mattype(self,contex):
+    props = bpy.context.scene.kiamaterialtools_oa
+    add(props.material_type)
+    #index = props.material_index
+
+def add(mattype):
+    if mattype == '':return
+
+    ui_list = bpy.context.window_manager.kiamaterialtools_list
+    itemlist = ui_list.itemlist
+
+    itemlist.clear()
+    for index in range(8):
+        item = itemlist.add()
+        item.name = str(index)
+
+        a = VERTEX_COLOR[ mattype ][ index ]
+        item.color = [conv(a[:2]) , conv(a[2:4]) , conv(a[4:]) , 1.0]
+
+        #item.color = (0.75,0.0,0.8,1.0)
+        #ui_list.active_index = len(itemlist) - 1
+
+def get_color_value(self):
+    return self.get('color', (1.0, 0.0, 1.0))
+
+def set_color_value(self, value):
+    self['color'] = value
+    #self['color_enum'] = 6
+
+
+#---------------------------------------------------------------------------------------
 
 def conv(v):
     return float(int(v,16))/255
@@ -42,17 +94,16 @@ def color_vertex(ob, vert, color,mode):
 #assign vertex color only selected vex
 #---------------------------------------------------------------------------------------
 def assign_vertex_color(mode):
-    global VERTEXCOLOR_BUFFER
-    props = bpy.context.scene.kiatools_oa
-    index = props.material_index
+    props = bpy.context.scene.kiamaterialtools_oa
+
+    ui_list = bpy.context.window_manager.kiamaterialtools_list
+    itemlist = ui_list.itemlist    
+    index = ui_list.active_index
 
     mat_type = props.material_type
 
-    if mat_type == 'BUFFER':
-        color = VERTEXCOLOR_BUFFER
-    else:
-        a = VERTEX_COLOR[ props.material_type ][ index ]
-        color = [conv(a[:2]) , conv(a[2:4]) , conv(a[4:]) , 1.0]
+    a = VERTEX_COLOR[ props.material_type ][ index ]
+    color = [conv(a[:2]) , conv(a[2:4]) , conv(a[4:]) , 1.0]
 
     utils.mode_o()
     for ob in utils.selected():
@@ -65,7 +116,6 @@ def assign_vertex_color(mode):
 #シェーダーはPrincipled BSDFである必要がある。
 #---------------------------------------------------------------------------------------
 def convert_vertex_color():
-    print('asss')
     for ob in utils.selected():
         for mat in ob.data.materials:
             nodes = mat.node_tree.nodes
@@ -77,15 +127,55 @@ def convert_vertex_color():
 #---------------------------------------------------------------------------------------    
 # First, select polugon face (not vertex) and execute this command.
 #---------------------------------------------------------------------------------------    
-VERTEXCOLOR_BUFFER = [0.0,0.0,0.0,1.0]
 def pick_vertex_color(mode):
-    global VERTEXCOLOR_BUFFER
+    props = bpy.context.scene.kiamaterialtools_oa
     mesh = bpy.context.object.data
-    utils.mode_o()
 
-    vcol_layer = mesh.vertex_colors.active
-    vtx_index = [loop_index for poly in mesh.polygons if poly.select for loop_index in poly.loop_indices]
+    #err0 = Msg("The selected object doesn't have a vertex color.")
+    if mode:
+        utils.mode_o()
+        vcol_layer = mesh.vertex_colors.active
 
-    VERTEXCOLOR_BUFFER = vcol_layer.data[vtx_index[0]].color[0:4]
-    #print(VERTEXCOLOR_BUFFER) 
-    utils.mode_e()
+        if vcol_layer == None:
+            #print('It do not have a vertex color.')
+            #err0.draw()
+            bpy.context.window_manager.popup_menu( msg01 , title="Error!", icon='INFO')
+            return
+
+        vtx_index = [loop_index for poly in mesh.polygons if poly.select for loop_index in poly.loop_indices]
+
+        if len(vtx_index) > 0:
+            props.material_color = vcol_layer.data[vtx_index[0]].color[0:4]
+            utils.mode_e()
+        else:
+            #err0.draw()
+            bpy.context.window_manager.popup_menu( msg02 , title="Error!", icon='INFO')
+
+    else:
+        for ob in utils.selected():
+            props = bpy.context.scene.kiamaterialtools_oa
+            color_vertex(ob, 2, props.material_color , 0)
+
+
+
+# class Msg:
+#     def __init__(self,text):
+#         self.text = text
+
+#     def draw(self):
+#         bpy.context.window_manager.popup_menu( self.msg , title="Error!", icon='INFO')
+
+#     def msg(self, context):
+#         layout= self.layout
+#         layout.label(text= self.text)
+
+#---------------------------------------------------------------------------------------    
+#Popup Error Message
+#---------------------------------------------------------------------------------------    
+def msg01(self, context):
+    layout= self.layout
+    layout.label(text= "The selected object doesn't have a vertex color.")
+
+def msg02(self, context):
+    layout= self.layout
+    layout.label(text= "You must select a face.")
